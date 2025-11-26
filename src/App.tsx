52739e2b65f5fa651aeb08395c5f1419c1e7c053
Marc <maruku@gmail.com>
2025-11-26 08:05:29 +0100

    feat: add header with breadcrumbs and main sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import React from 'react'
 import '@/App.css'
-import {Outlet} from "react-router";
+import { Outlet, useLocation } from "react-router";
+import { Link } from 'react-router-dom';
 
 const App = (): React.JSX.Element => {
+  const location = useLocation()
+
+  const breadcrumbs = location.pathname === '/' ? ['List'] : ['List', 'Details']
+
   return (
     <div className="app">
-      <Outlet />
+      <header className="header">
+        <div className="header__left">
+          <Link to="/" className="home-link">Home</Link>
+          <nav className="breadcrumbs">
+            {breadcrumbs.map((crumb, i) => (
+              <span key={i}>{crumb}</span>
+            ))}
+          </nav>
+        </div>
+        <div className="header__right">
+          <span>🛒 {"<Cart Count goes here>"}</span>
+        </div>
+      </header>
+      <main className="main">
+        <Outlet />
+      </main>
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './main.css'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
